70e8860ebf6c3b099b923f57ea309f0c8b116dfd
nurstan28 <101414292+nurstan28@users.noreply.github.com>
2022-03-15 21:26:14 +0600

    Add files via upload

---
+++ day_2/work3.py
@@ -0,0 +1,16 @@
+print('macBook Air')
+
+os = 'OS: macOS'
+print(os)
+
+display = 'display: retina'
+print(display)
+
+ram = 'ram: 8gb'
+print(ram)
+
+rom = 'rom: SSD 256gb'
+print(rom)
+
+year = 'year of issue: 2021'
+print(year)
